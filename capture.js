649177abe5ea5ec6ea8a16a8556919bee385074f
wolfitis <webblog123@gmail.com>
2020-12-28 23:24:55 -0500

    funcitonality added for capturing snap

--- capture.js
+++ capture.js
@@ -6,22 +6,61 @@
 
     var video = null;
     var canvas = null;
-    var photo = null;
+    var snap = null;
     var capturebtn = null;
 
-})
-function startup() {
-    video = document.getElementById('video');
-    canvas = document.getElementById('canvas');
-    photo = document.getElementById('photo');
-    capturebtn = document.getElementById('capturebtn');
+    function startup() {
+        video = document.getElementById('video');
+        canvas = document.getElementById('canvas');
+        snap = document.getElementById('snap');
+        capturebtn = document.getElementById('capturebtn');
 
-}
-navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(function (stream) {
-        video.srcObject = stream;
-        video.play();
-    })
-    .catch(function (err) {
-        console.log("An error occurred: " + err);
-    });
+        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+            .then(function (stream) {
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(function (err) {
+                console.log("An error occurred: " + err);
+            });
+        video.addEventListener('canplay', function (ev) {
+            if (!streaming) {
+                height = video.videoHeight / (video.videoWidth / width);
+
+                video.setAttribute('width', width);
+                video.setAttribute('height', height);
+                canvas.setAttribute('width', width);
+                canvas.setAttribute('height', height);
+                streaming = true;
+            }
+        }, false);
+
+        capturebtn.addEventListener('click', function (ev) {
+            takesnap();
+            ev.preventDefault();
+        }, false);
+
+        clearsnap();
+
+    }) ();
+    function clearsnap() {
+        var context = canvas.getContext('2d');
+        context.fillStyle = "#AAA";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        var data = canvas.toDataURL('image/png');
+        snap.setAttribute('src', data);
+    }
+    function takesnap() {
+        var context = canvas.getContext('2d');
+        if (width && height) {
+            canvas.width = width;
+            canvas.height = height;
+            context.drawImage(video, 0, 0, width, height);
+
+            var data = canvas.toDataURL('image/png');
+            snap.setAttribute('src', data);
+        } else {
+            clearsnap();
+        }
+    }
